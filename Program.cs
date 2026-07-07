13a34f8ef52106fd0a373a22b34d5ec4e6505fb9
YudaFirdaus <yudaf2429@gmail.com>
2026-07-07 14:51:32 +0700

    Hari 7: Mini proyek RPG - Review Minggu 1

--- Program.cs
+++ Program.cs
@@ -1,23 +1,64 @@
 using System;
+using System.Collections.Concurrent;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
 
 class Hari6
 {
   static void Main()
   {
-   System.Console.WriteLine("Masukkan kalimat: ");
-   string kalimat = Console.ReadLine();
-   System.Console.WriteLine("=== Analisis Kalimat ===");
-   System.Console.WriteLine("Kalimat asli: " + kalimat);
-   System.Console.WriteLine("Huruf kapital semua: " + kalimat.ToUpper());
-   System.Console.WriteLine("Panjang kalimat: " + kalimat.Length + " karakter");
-   if (kalimat.Contains("musuh") || kalimat.Contains("Musuh"))
+    System.Console.WriteLine("=== GAME RPG SEDERHANA ===");
+    System.Console.WriteLine("Masukkan nama karaktermu: ");
+    string nama = Console.ReadLine();
+    System.Console.WriteLine("Pilih kelas (Warrior/Mage/Archer): ");
+    string kelas = Console.ReadLine();
+
+    int hp = 0;
+    int attack = 0;
+    string[] skill = {};
+
+    if (kelas.ToLower() == "warrior")
     {
-      System.Console.WriteLine("Mengandung kata 'musuh': Ya!");
+      hp = 100;
+      attack = 30;
+      skill = new string [] {"Slash", "Shild Bash", "War Cry"};
     }
-    else
+    else if (kelas.ToLower() == "mage")
     {
-      System.Console.WriteLine("Mengandung kata 'musuh': Tidak!");
+      hp = 70;
+      attack = 50;
+      skill = new string [] {"Fireball", "Ice Spike", "Thunder"};
     }
-    System.Console.WriteLine("Huruf pertama kapital: " + kalimat.Substring(0, 1).ToUpper() + kalimat.Substring(1));
+    else if (kelas.ToLower() == "archer")
+    {
+      hp = 85;
+      attack = 45;
+      skill = new string [] {"Arrow Shot", "Multi Shot", "Eagele Eye"};
+    }
+
+    System.Console.WriteLine("\n=== Karakter Dibuat! ===");
+    System.Console.WriteLine("Nama: " + nama);
+    System.Console.WriteLine("Kelas: " + kelas);
+    System.Console.WriteLine("HP: " + hp);
+    System.Console.WriteLine("Attack: " + attack);
+
+    System.Console.WriteLine("\n=== Skill " + kelas + " ===");
+    for (int i = 0; i < skill.Length; i++)
+    {
+      System.Console.WriteLine((i + 1) + ". " + skill[i]);
+    }
+
+    System.Console.WriteLine("\n=== Simulasi Pertarungan ===");
+    System.Console.WriteLine(nama + " menyerang musuh!");
+    int HPmusuh = 100;
+    int giliran = 1;
+    while (HPmusuh > 0)
+    {
+      HPmusuh -= attack;
+      if (HPmusuh < 0) HPmusuh = 0;
+      System.Console.WriteLine("Giliran " + giliran + " - Hp Musuh: " + HPmusuh);
+      giliran++;
+    }
+    System.Console.WriteLine("Musuh dikalahkan dalam " + (giliran -1) + " giliran!");
   }
 }
